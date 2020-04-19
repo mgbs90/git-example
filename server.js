12962bdc2f31f5afd53d8bea22d2de2655c47423
mohammad gharebaghi <gharebaghii90@gmail.com>
2020-04-19 13:11:57 +0430

    change port

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000;
+
 app.get('/', (req, res) => {
     res.send('hello to node js git repasitory.');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server runing on port 3000');
 });
